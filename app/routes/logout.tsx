7bd45758c6dfbeaa89a9a36bdf4cb47580d7f6dd
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2023-08-27 22:14:24 -0400

    final cleanup

--- app/routes/logout.tsx
+++
@@ -1,7 +0,0 @@
-import { type LoaderArgs } from '@remix-run/node';
-
-import { deleteSession } from '~/lib/session';
-
-export function loader({ request }: LoaderArgs) {
-	return deleteSession(request);
-}
